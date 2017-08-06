23e7a4b9808d6a1f2494d9b4509bff2307aa37e5
Tyler Shaw <tyler.shaw@acaexpress.local>
2017-08-05 22:48:22 -0500

    Init Commit

---
+++ Ethereum.web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Ethereum.web.MvcApplication" Language="C#" %>
---
+++ Ethereum.web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Ethereum.web.Startup))]
+namespace Ethereum.web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
