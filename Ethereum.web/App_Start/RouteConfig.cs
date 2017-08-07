43eab145d5487ba7ef05fd3ca0d091f338b24bcb
Tyler Shaw <tyler.shaw@acaexpress.local>
2017-08-07 11:57:01 -0500

    ignoreRoutes

--- Ethereum.web/App_Start/RouteConfig.cs
+++ Ethereum.web/App_Start/RouteConfig.cs
@@ -11,7 +11,7 @@ namespace Ethereum.Web
     {
         public static void RegisterRoutes(RouteCollection routes)
         {
-            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+            routes.IgnoreRoute("");
 
             routes.MapRoute(
                 name: "Default",
